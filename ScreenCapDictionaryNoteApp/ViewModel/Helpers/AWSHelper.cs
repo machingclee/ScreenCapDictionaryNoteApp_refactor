81c5c6afadd1f62721f8d097b061ef51d6531bc8
Ching-Cheong Lee <machingclee@gmail.com>
2020-02-01 05:14:18 +0800

    migrate to .net core 3.1 from .net framework4.7.2

---
+++ ScreenCapDictionaryNoteApp/ViewModel/Helpers/AWSHelper.cs
@@ -0,0 +1,27 @@
+﻿
+using Amazon;
+using Amazon.Runtime.CredentialManagement;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScreenCapDictionaryNoteApp.ViewModel.Helpers
+{
+    public class AWSHelper
+    {
+        public AWSHelper()
+        {
+            var options = new CredentialProfileOptions
+            {
+                AccessKey = "",
+                SecretKey = ""
+            };
+            var profile = new Amazon.Runtime.CredentialManagement.CredentialProfile("basic_profile", options);
+            profile.Region = RegionEndpoint.USWest1;
+            var netSDKFile = new NetSDKCredentialsFile();
+            netSDKFile.RegisterProfile(profile);
+        }
+    }
+}
